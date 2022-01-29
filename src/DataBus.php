6bdf9511ae290f69877e0b9d30ba0c6ebd6b1335
Fedor B Gorsky <fedor@support-pc.org>
2022-01-29 10:14:12 +0300

    DataBus is singleton

--- src/DataBus.php
+++ src/DataBus.php
@@ -2,8 +2,11 @@
 
 namespace fgh151\tg;
 
+use Exception;
+
 abstract class DataBus
 {
+    protected static $instances = [];
     /** @var QueueClientInterface | AwsClient */
     protected $ymq;
 
@@ -20,5 +23,28 @@ abstract class DataBus
     public function init()
     {
 
+    }
+
+    abstract public function run();
+    abstract public function onMessage(string $queueUrl, $fn): DataBus;
+    abstract public function fetchUrls($fn, $params): DataBus;
+
+    protected static function getInstance(): DataBus
+    {
+        $cls = static::class;
+        if (!isset(self::$instances[$cls])) {
+            self::$instances[$cls] = new static();
+        }
+
+        return self::$instances[$cls];
+    }
+
+    public function __wakeup()
+    {
+        throw new Exception("Cannot unserialize a singleton.");
+    }
+
+    protected function __clone()
+    {
     }
 }
